ddaab263de672b602872e5393c998e4592ef5783
Tjeerd Jan <Tjeerd.jan.van.der.Molen@ordina.nl>
2016-03-30 21:47:15 +0200

    * fixes

--- connect.sh
+++ connect.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-ssh -R 8080:localhost:8080 core@k8s-test.cloudapp.net -p 2022
+ssh core@k8s-test.cloudapp.net -p 2022
--- get_kubectl.sh
+++ get_kubectl.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-curl -O https://storage.googleapis.com/kubernetes-release/release/v1.1.8/bin/linux/amd64/kubectl
+curl -O https://storage.googleapis.com/kubernetes-release/release/v1.2.0/bin/linux/amd64/kubectl
