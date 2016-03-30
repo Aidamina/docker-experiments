b84e9442c91598913c23d3d988c0005f6161099b
Tjeerd Jan <Tjeerd.jan.van.der.Molen@ordina.nl>
2016-03-30 17:57:17 +0200

    * update

--- prepare_nodes.sh
+++ prepare_nodes.sh
@@ -5,5 +5,8 @@ mkdir -p /etc/kubernetes/ssl
 cd units
 #Make network environment variables available on system
 fleetctl start setup-network-environment.service
+fleetctl start kube-flanneld.service
+fleetctl start generate-serviceaccount-key.service
+
 
 /usr/bin/etcdctl set /coreos.com/network/config '{"Network":"10.244.0.0/16", "Backend": {"Type": "vxlan"}}'
