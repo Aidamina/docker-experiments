11748cabf97135c410bfb709347f11f8cbed3cbc
Tjeerd Jan <Tjeerd.jan.van.der.Molen@ordina.nl>
2016-03-30 19:03:42 +0200

    * worker

--- prepare_nodes.sh
+++ prepare_nodes.sh
@@ -1,12 +1,28 @@
 #!/bin/bash
 
-mkdir -p /etc/kubernetes/ssl
+SERVICE=k8s-test
+
 
 cd units
-#Make network environment variables available on system
+# Make network environment variables available on system
+
+# Global units
 fleetctl start setup-network-environment.service
+
+# prerequisite for flanneld (obsolete now as its included in the unit file)
+# /usr/bin/etcdctl set /coreos.com/network/config '{"Network":"10.244.0.0/16", "Backend": {"Type": "vxlan"}}'
+
 fleetctl start kube-flanneld.service
+
+# Master units
 fleetctl start generate-serviceaccount-key.service
+fleetctl start kube-apiserver.service
+fleetctl start kube-controllermanager.service
+fleetctl start kube-scheduler.service
 
+# Worker units
 
-/usr/bin/etcdctl set /coreos.com/network/config '{"Network":"10.244.0.0/16", "Backend": {"Type": "vxlan"}}'
+sed -e "s/<master-private-ip>/$SERVICE.cloudapp.net/g" kube-proxy.template.service > kube-proxy.service
+sed -e "s/<master-private-ip>/$SERVICE.cloudapp.net/g" kube-kubelet.template.service > kube-kubelet.service
+fleetctl start kube-proxy.service
+fleetctl start kube-kubelet.service
